e06fae9f8fe991f1cc9ba27153a57c88be94450c
prajjwal poudel <iamprazol@gmail.com>
2020-04-09 17:20:50 +0545

    Header, Navbar and pages created

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div className="jumbotron">
+      <h1>About</h1>
+      <p> This app uses React, Redux, React Router libraries;</p>
+    </div>
+  );
+};
+
+export default AboutPage;
